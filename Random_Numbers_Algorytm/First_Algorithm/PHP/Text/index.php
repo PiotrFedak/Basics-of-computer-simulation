eadfd7e5e3279dfa8d4afc384282a616bd6eff61
PiotrFedak <piotr.fedak@studenci.collegiumwitelona.pl>
2024-03-13 14:47:14 +0100

    First task MVP

--- Random_Numbers_Algorytm/First_Algorithm/PHP/Text/index.php
+++ Random_Numbers_Algorytm/First_Algorithm/PHP/Text/index.php
@@ -5,32 +5,35 @@ function generatePoints() {
         $stop=0;
         $Xo=1;
         $a=2;
-        $m=10000;
+        $m=1000;
         $modulo=0;
         $x=0;
         do{
             $x=$Xo*$a;
             if($x<$m){
+                $result=$x/$m;
                 foreach($points as $point){
-                   if($point[0]==$x && $point[1]==$m){
+                   if($point[0]==$result){
                     $stop=1;
                    }
                 }
                 if($stop!==1){
-                $points[]= [$x,$m];
+                $result=$x/$m;
+                $points[]= [$result];
                 $Xo=$x;
                 }
             }
             if($x>$m){
                 $modulo=$x%$m;
                 $x=$modulo;
+                $result=$x/$m;
                 foreach($points as $point){
-                    if($point[0]==$x && $point[1]==$m){
+                    if($point[0]==$result){
                      $stop=1;
                     }
                  }
                  if($stop!==1){
-                 $points[]= [$x,$m];
+                $points[]= [$result];
                  $Xo=$x;
                  }
             }
@@ -41,11 +44,26 @@ function generatePoints() {
 
 
     $points = generatePoints();
+    $suma=0;
+    $all=0;
+    $table = [];
 
 foreach ($points as $point) {
-    echo $point[0].(" ");
-    echo $point[1]. "\n";
+    echo $point[0].(" ") ."\n";
+}
+foreach ($points as $point) {
+   $suma+= $point[0];
+   $all+=1;
+}
+$average=$suma/$all;
+
+echo $average.("  śrenia  dla ").$all."\n";
+
+for ($i=0; $i<$all-1; $i++ ) {
+    $table[]=[$points[$i],$points[$i+1]];
 }
 
-
-?>
+foreach ($table as $tab) {
+    echo $tab[0][0].(" ") ."\n";
+    echo $tab[1][0].(" ") ."\n";
+}
